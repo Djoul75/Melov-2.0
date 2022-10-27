1868d1f881615ffc19785914a6f4e556deada691
Jules Gorce <jules.gorce@gmail.com>
2022-10-27 12:17:30 +0200

    omniauth with spotify ok

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def spotify
+    @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    # Now you can access user's private data, create playlists and much more
+    @user = User.find_or_initialize_by(email: @spotify_user.email)
+    # Access private data
+    @user.spotify_id = @spotify_user.id
+    @user.spotify_token = @spotify_user.id
+    @user.token = @spotify_user.credentials["token"]
+    @user.spotify_json = @spotify_user.as_json
+
+    if @user.new_record?
+      @user.password = Devise.friendly_token
+      redirection_url = edit_user_registration_path
+    else
+      redirection_url = root_path
+    end
+    @user.save
+    sign_in @user
+
+    redirect_to redirection_url
+  end
+
+#  méthode ci-dessous pour récupérer la track jouée, copié collé de la doc, en dévelopement
+#  def currently_playing
+#    url = "me/player/currently-playing"
+#    response = RSpotify.resolve_auth_request(@user.id, url)
+#    return response if RSpotify.raw_response
+
+#    Track.new response["item"]
+#  end
+
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+
+  # More info at:
+  # https://github.com/heartcombo/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
+end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_ID"], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify', token_params: { parse: :json }
 end
 
 OmniAuth.config.allowed_request_methods = [:post, :get]
