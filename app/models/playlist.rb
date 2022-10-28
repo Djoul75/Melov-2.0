56db00888d2c1868202116bddde66093832417f5
Jules Gorce <jules.gorce@gmail.com>
2022-10-28 11:03:28 +0200

    add has_many to playlist model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,6 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+  has_many :buddies, dependent: :destroy
+  has_many :playlist_songs, dependent: :destroy
+  has_many :songs, through: :playlist_songs
 end
